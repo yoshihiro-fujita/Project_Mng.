df60dde0b9d24b38666bedaa4e3fbdc8d8761f06
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-07-22 10:31:07 +0900

    start Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get '/signup', to: 'users#new'
 root 'application#hello'
 end
