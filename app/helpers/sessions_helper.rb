02b5f8e1d0e197afd3690d09c9b147effba61d65
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-07-25 19:47:16 +0900

    implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,14 @@ module SessionsHelper
     if session[:user_id]
         @current_user ||= User.find_by(id:session[:user_id]) 
     end
+    end
 
     def logged_in?
         !current_user.nil?
+    end
+
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
-  get 'sessions/new'
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get '/signup', to: 'users#new'
