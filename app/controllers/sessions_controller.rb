4f08405a82776b2f0b3634a87d9cad6c4a21279c
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-07-25 21:57:37 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ def create
   user = User.find_by(email: params[:session][:email].downcase)
   if user && user.authenticate(params[:session][:password])
     log_in user
+    params[:session][:remember_me] =='1'? remember(user):forget(user)
     redirect_to user
   
   else
@@ -15,7 +16,7 @@ def create
 end
 
 def destroy
-  log_out
+  log_out if looged_in?
   redirect_to root_url
 end
 end
